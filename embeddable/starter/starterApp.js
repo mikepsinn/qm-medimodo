1714b66f128f850afaafe080f75f97a39920ec1c
Mike Sinn <m@quantimodo.com>
2018-06-16 04:57:24 +0000

    Merge pull request #1707 from mikepsinn/develop

--- embeddable/starter/starterApp.js
+++ embeddable/starter/starterApp.js
@@ -13,10 +13,8 @@ angular.module('qmStarterApp',
         'qmAddMeasurement',
         'jtt_wikipedia'
     ])
-
     //application states
     .config(function ($stateProvider) {
-
         $stateProvider
         //root route should navigate to main template and controller of current plugin
             .state('login', {
@@ -30,24 +28,12 @@ angular.module('qmStarterApp',
     })
 
     .config(function (settingsProvider) {
-
         var currentDomainWithPort = settingsProvider.extractDomainWithPort(window.location.href);
         var currentDomainWithoutPort = settingsProvider.stripPort(currentDomainWithPort);
-
-        //if app hosted on staging
-        if (currentDomainWithoutPort == 'staging.quantimo.do') {
-            //set apiHost to call staging API
-            settingsProvider.setSettings({
-                apiHost: 'https://staging.quantimo.do/'
-            });
+        if (currentDomainWithoutPort.indexOf('staging') !== -1) {  //if app hosted on staging
+            settingsProvider.setSettings({apiHost: 'https://staging.quantimo.do/'});
         }
-
-        //if app in development
-        if (currentDomainWithoutPort == 'local.quantimo.do') {
-            //set apiHost to call staging API
-            settingsProvider.setSettings({
-                apiHost: 'https://local.quantimo.do/'
-            });
+        if (currentDomainWithoutPort.indexOf('local') !== -1) {  //if app in development
+            settingsProvider.setSettings({apiHost: 'https://local.quantimo.do/'});
         }
-
     });
